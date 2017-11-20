4e5439efae0032ac1f7239ebc4dd181dc6f8bfa8
Owen Gombas <owen.gombas@gmail.com>
2017-11-20 12:04:21 +0100

    Pagination counter

--- admin.php
+++ admin.php
@@ -196,15 +196,18 @@
                         <div class="row margin-top-25">
                             <div class="col-md-12">
                                 <div class="row">
-                                    <div class="col-md-2">
+                                    <div class="col-md-1">
                                     </div>
                                     <div class="col-md-4">
                                         <a href="admin.php?page=<?= $page - 1 < 1 ? $page : $page - 1 ?>" class="btn btn-secondary btn-block <?= $page - 1 < 1 ? 'disabled' : '' ?>">Previous</a>
                                     </div>
+                                    <div class="col-md-2">
+                                        <h3 class="text-center"><?= $page.'/'.$total ?></h3>
+                                    </div>
                                     <div class="col-md-4">
                                         <a href="admin.php?page=<?= $page + 1 > $total ? $page : $page + 1 ?>" class="btn btn-secondary btn-block <?= $page + 1 > $total ? 'disabled' : '' ?>">Next</a>
                                     </div>
-                                    <div class="col-md-2">
+                                    <div class="col-md-1">
                                     </div>
                                 </div>
                             </div>
